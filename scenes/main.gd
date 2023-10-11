3d8949edbfec9a3c0d9862ff1983b57a0021168a
Toxe <toxe@toxe.net>
2023-10-11 19:24:36 +0200

    Define input actions

--- scenes/main.gd
+++ scenes/main.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-    if event.is_action_pressed("ui_cancel"):
+    if event.is_action_pressed("quit"):
         get_tree().quit()
 
 
