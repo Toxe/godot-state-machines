804ddb847a8bd1d014f9643b662949c1d257fff9
Toxe <toxe@toxe.net>
2023-10-13 13:13:40 +0200

    Guard: "follow_player" state

--- scenes/blue_guard/blue_guard.gd
+++ scenes/blue_guard/blue_guard.gd
@@ -13,19 +13,31 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta: float) -> void:
     if player_in_range:
         player_detected = false
+
         for raycast: RayCast2D in [$PlayerDetector/RayCast2D1, $PlayerDetector/RayCast2D2, $PlayerDetector/RayCast2D3]:
-            var player := raycast.get_collider() as Player
-            if player:
+            assert(raycast.enabled)
+
+            var collider := raycast.get_collider()
+            if collider and collider is Player:
                 player_detected = true
                 break
+
+        var player := get_tree().get_first_node_in_group("player") as Player
+        $PlayerDetector.look_at(player.position)
 
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
     if body is Player:
         player_in_range = true
 
+        for raycast: RayCast2D in [$PlayerDetector/RayCast2D1, $PlayerDetector/RayCast2D2, $PlayerDetector/RayCast2D3]:
+            raycast.enabled = true
+
 
 func _on_player_detector_body_exited(body: Node2D) -> void:
     if body is Player:
         player_in_range = false
         player_detected = false
+
+        for raycast: RayCast2D in [$PlayerDetector/RayCast2D1, $PlayerDetector/RayCast2D2, $PlayerDetector/RayCast2D3]:
+            raycast.enabled = false
--- scenes/blue_guard/states/follow_player.gd
+++ scenes/blue_guard/states/follow_player.gd
@@ -1,7 +1,59 @@
 extends State
 
+const speed := 100.0
+
 @export var player_lost_state: State = null
+
+var move_to := Vector2.ZERO
+var player_got_away := false
 
 
 func _ready() -> void:
     assert(player_lost_state != null)
+
+
+func enter() -> void:
+    var guard := owner as BlueGuard
+    guard.get_node("AnimationPlayer").play("run")
+
+    move_to = get_player_position()
+
+
+func physics_process(_delta: float) -> State:
+    var guard := owner as BlueGuard
+
+    if player_got_away:
+        return player_lost_state
+
+    if guard.player_detected:
+        move_to = get_player_position()
+        $PlayerLostTimer.stop()
+    else:
+        if $PlayerLostTimer.is_stopped():
+            $PlayerLostTimer.start()
+
+    guard.velocity = guard.position.direction_to(move_to) * speed
+    guard.look_at(move_to)
+
+    update_line()
+
+    if guard.position.distance_to(move_to) > 5:
+        guard.move_and_slide()
+
+    return null
+
+
+func get_player_position() -> Vector2:
+    var player := get_tree().get_first_node_in_group("player") as Player
+    return player.position
+
+
+func update_line() -> void:
+    var guard := owner as BlueGuard
+    $Line2D.points[0] = guard.position + guard.transform.x * 30.0
+    $Line2D.points[1] = move_to
+    $Line2D.visible = guard.position.distance_to($Line2D.points[1]) > 30.0
+
+
+func _on_player_lost_timer_timeout() -> void:
+    player_got_away = true
