bfb47aacfba9b2f796854d25a800b27940dcbf94
Toxe <toxe@toxe.net>
2023-10-14 16:15:24 +0200

    Get player only once in setup()

--- scenes/player/state/walk.gd
+++ scenes/player/state/walk.gd
@@ -2,9 +2,14 @@ extends State
 
 const speed := 300.0
 
+var player: Player = null
+
+
+func setup(_sm: StateMachine) -> void:
+    player = owner as Player
+
 
 func physics_process(_delta: float) -> State:
-    var player := owner as Player
     player.velocity = Input.get_vector("move_left", "move_right", "move_up", "move_down") * speed
     player.move_and_slide()
     return null
